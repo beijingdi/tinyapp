f1ff9ff145a679dd696f5d934f32fd6ede53a82c
Di <dwu233@gmail.com>
2022-04-20 21:41:50 +0000

    additional features

--- express_server.js
+++ express_server.js
@@ -34,9 +34,17 @@ const hasEmail = (email) => {
 };
 
 
+
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+        longURL: "https://www.tsn.ca",
+        userID: "aJ48lW"
+    },
+  i3BoGr: {
+        longURL: "https://www.google.ca",
+        userID: "aJ48lW"
+    }
 };
 
 app.get("/", (req, res) => {
@@ -55,11 +63,21 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { user: users[req.cookies['user_id']] };
+  if (req.cookies['user_id']) {
+    console.log(req.cookies['user_id']);
+    res.status(400);
+    res.send("please register first");
+    return res.render("urls_new", templateVars);
+  }
+  res.render("user_login", templateVars);
+  
 });
 
-app.post("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] , user: users[req.cookies['user_id']]};
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"] , user: users[req.cookies['user_id']]};
   res.render("urls_show", templateVars);
 });
 
@@ -68,10 +86,10 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
+// app.post("/urls", (req, res) => {
+//   console.log(req.body);  // Log the POST request body to the console
+//   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+// });
 
 app.post("/urls/:shortURL/delete", (req,res) => {
   delete urlDatabase[req.params.shortURL];
